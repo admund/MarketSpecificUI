35e6aee27263f6967c2feff7d4e0f48937305ea6
Adrian Gardian <adrian.gardian@brainly.com>
2024-02-03 12:01:08 +0100

    publishing to MavenCentral

--- marketspecificui/build.gradle.kts
+++ marketspecificui/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
+    id("com.vanniktech.maven.publish") version "0.27.0"
 }
 
 android {
@@ -35,6 +38,52 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.6"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Repo"
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
+}
+
+
+mavenPublishing {
+//    publishToMavenCentral(SonatypeHost.DEFAULT)
+    publishToMavenCentral(SonatypeHost.S01)
+
+    signAllPublications()
+
+//    coordinates("io.github.admund", "marketspecificui", "0.1.0-SNAPSHOT")
+    coordinates("io.github.admund", "marketspecificui", "0.1.0")
+
+    pom {
+        name.set("MarketSpecificUI")
+        description.set("The library is for helping manage market-specific resources for Android apps.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/admund/MarketSpecificUI")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("admund")
+                name.set("Adrian Gardian")
+                url.set("https://github.com/admund")
+            }
+        }
+        scm {
+            url.set("https://github.com/admund/MarketSpecificUI")
+            connection.set("scm:git:git://github.com/admund/MarketSpecificUI.git")
+            developerConnection.set("scm:git:ssh://git@github.com/admund/MarketSpecificUI.git")
+        }
     }
 }
 
